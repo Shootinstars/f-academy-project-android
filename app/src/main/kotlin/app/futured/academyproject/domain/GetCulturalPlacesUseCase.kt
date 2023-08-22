57502a947f4feae571958aa5e20ac75272b47f39
Martin Horák <536446@mail.muni,cz>
2023-08-22 10:05:02 +0200

    Lesson 6

--- app/src/main/kotlin/app/futured/academyproject/domain/GetCulturalPlacesUseCase.kt
+++ app/src/main/kotlin/app/futured/academyproject/domain/GetCulturalPlacesUseCase.kt
@@ -13,5 +13,19 @@ class GetCulturalPlacesUseCase @Inject constructor(
     //  Implementuj metódu build, ktorá vráti zoznam miest.
     //  Využi ApiManager a namapuj CulturalPlaces na List<Place>.
     //  Mali by stačiť iba nasledujúce properties: id, longitude, latitude, name, type, note,
-    override suspend fun build(args: Unit): List<Place> = TODO("Just keep coding, just keep coding...")
+    override suspend fun build(args: Unit): List<Place> =
+        apiManager.getCulturalPlaces().features.map {
+            Place(
+                id = it.properties.ogcFid,
+                name = it.properties.name,
+                type = it.type,
+                email = it.properties.email,
+                phone = it.properties.phone,
+                image1Url = it.properties.image1Url,
+                webUrl = it.properties.webUrl,
+                latitude = it.geometry?.coordinates?.getOrNull(0),
+                longitude = it.geometry?.coordinates?.getOrNull(1),
+                note = it.properties.note
+            )
+        }
 }
